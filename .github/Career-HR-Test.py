d290e5f0d3025e70b228ace6e53bdc61e26b40d2
Jonas Dixon <[jonas.dixon.42@gmail.com]}git config --global user.email [jonas.dixon.42@gmail.com]>
2022-06-30 21:14:12 +0000

    Update names and files

---
+++ .github/Career-HR-Test.py
@@ -0,0 +1,14 @@
+import unittest
+from Career-Home-Runs import find_player
+
+
+class TestFileName(unittest.TestCase):
+    def test_function1(self):
+        self.assertEqual(function1(), 0)
+
+    def test_function2(self):
+        self.assertEqual(function2(2,1), 3)
+        self.assertEqual(function2(2.1, 1.2), 3.3)
+
+if __name__ == '__main__':
+    unittest.main()
