cb0e3cc5ef16e754e25f88d5e0768da29a6a7b78
Jonas Dixon <[jonas.dixon.42@gmail.com]}git config --global user.email [jonas.dixon.42@gmail.com]>
2022-07-06 19:38:21 +0000

    Updated Home Run file

--- .github/Career-Home-Runs.py
+++ .github/Career-Home-Runs.py
@@ -1,24 +1,38 @@
 import statsapi
+import sqlalchemy as db
+import pandas as pd
 
-def find_player():
-  player = input('Enter a position player (Firstname Lastname): ')
-  player_count = 0
-  number = 1
-  for player in statsapi.lookup_player(player):
-      ++player_count
-  if player_count > 1:
-    for player in statsapi.lookup_player(player):
-      print(f'{number}. Full name: {player['fullName']}, Position: {player['primaryPosition']['abbreviation']}')
-      ++number
-    player_num = input('Enter a number to specify desired player above: ')
-    player = player[player_num - 1]['id'])
-    return player
-  else:
-    if len(player.split()) != 2:
-      return "Please enter player name in following format: Firstname Lastname"
-    if statsapi.lookup_player(player).position == 'P' | 'SP' | 'RP':
-      return "Player entered was not a position player"
+def validate_name(player):
+  # Validates player from user to look up in the API
+  while player.isdigit() or player == "":
+    player = input("Please enter player name in following format: Lastname")
   return player
 
-def find_career_high_hrs(player):
-  
+def get_player_ids(player):
+  players = statsapi.lookup_player(player)
+  player_ids = []
+  for id_ in players:
+    player_ids.append(id_['id'])
+  return player_ids
+
+def ids_to_stats(ids):
+  player_stats_list = []
+
+  for player_id in ids:
+    try:
+      stats = statsapi.player_stat_data(player_id, group = 'homeRuns', type = 'career')['stats'][0]['stats']
+      stats.update({'player' : statsapi.lookup_player(player_id)[0]['fullName']})
+      player_stats_list.append(stats)
+    except:
+      pass
+    print(pd.DataFrame(player_stats_list))
+
+def main():
+  player_name = input('Enter an MLB player (Lastname): ')
+  valid_name = validate_name(player_name)
+  ids = get_player_ids(valid_name)
+  ids_to_stats(ids)
+  # print(statsapi.league_leaders('homeRuns', season=2019, playerPool = 'rookies', limit = 5))
+
+if __name__ == "__main__":
+  main()
