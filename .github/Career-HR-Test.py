1fb1620cb91386b8eaf80bd3ca37616c929b2bbf
Jonas Dixon <[jonas.dixon.42@gmail.com]}git config --global user.email [jonas.dixon.42@gmail.com]>
2022-07-07 15:01:38 +0000

    Changed test file

--- .github/Career-HR-Test.py
+++ .github/Career-HR-Test.py
@@ -1,14 +1,28 @@
 import unittest
-from Career-Home-Runs import find_player
+from Career-Home-Runs import *
 
 
 class TestFileName(unittest.TestCase):
-    def test_function1(self):
-        self.assertEqual(function1(), 0)
+    def test_validate_name(self):
+        self.assertEqual(validate_name('Judge'), 'Judge')
+        self.assertEqual(validate_name('Ramirez')), 'Ramirez')
+    
+    def test_active_players(self):
+        self.assertEqual(ids_to_stats(get_player_ids('Ramirez')).groupby('player')
+                         .sum().sort_values('homeRuns', ascending = False)['homeRuns'], 
+                         'player
+                          Jose Ramirez      179
+                          Harold Ramirez     22
+                          Erasmo Ramirez      0
+                          Noe Ramirez         0
+                          Yohan Ramirez       0
+                          Name: homeRuns, dtype: int64')
+    
+    def test_inactive_players(self):
+        self.assertEqual()
 
-    def test_function2(self):
-        self.assertEqual(function2(2,1), 3)
-        self.assertEqual(function2(2.1, 1.2), 3.3)
+    def test_matching_last_name(self):
+        self.assertEqual(, 0)
 
 if __name__ == '__main__':
     unittest.main()
